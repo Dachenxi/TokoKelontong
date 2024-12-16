a9f819d8acb77c18d9598968a0eb2b382144f71f
Putra <70789166+Dachenxi@users.noreply.github.com>
2024-12-16 20:51:17 +0700

    remove unsued repotr

--- app/view_all_tabel.py
+++ app/view_all_tabel.py
@@ -2,7 +2,6 @@ from rich import print,box
 from rich.panel import Panel
 from rich.columns import Columns
 from rich.align import Align
-from rich.live import Live
 from database import execute_query,connect_db
 from config import load_environ
 from view import tabel,loading
@@ -17,7 +16,7 @@ def nama_kolom(namaTabel):
     return result
 
 def generate_tabel(nama:str):
-    query = f"SELECT * FROM {nama}"
+    query = f"SELECT * FROM {nama} "
     result = execute_query(query=query)
     return tabel(title=f"View Table {nama}",kolom=nama_kolom(f"{nama}"),
                   baris=result)
