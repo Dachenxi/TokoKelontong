0270147a9003e10861a17ddcd175d656bedadb8d
Putra <70789166+Dachenxi@users.noreply.github.com>
2024-12-16 20:51:32 +0700

    adding all tabel

--- app/view_barang.py
+++ app/view_barang.py
@@ -1,2 +1,25 @@
+from view import tabel,loading
+from database import execute_query
+from rich import print
+
 def main():
-    pass
+    loading([
+        ("Mengambil Data Barang", 0.5)
+    ])
+    query = "SELECT * FROM barang"
+    header_tabel = ["ID",
+                    "ID Kategori",
+                    "ID Supplier",
+                    "Nama",
+                    "Harga Jual",
+                    "Harga Pack"
+                    ]
+    print(
+        execute_query(query=query)
+    )
+    print(tabel("Barang",
+                kolom=header_tabel,
+                baris=execute_query(query=query)
+                )
+          )
+    
