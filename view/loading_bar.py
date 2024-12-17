ff08d56550d6e3e5759ab3b76d159e22c896cedd
Putra <70789166+Dachenxi@users.noreply.github.com>
2024-12-17 08:17:38 +0700

    update loading_bar.py

--- view/loading_bar.py
+++ view/loading_bar.py
@@ -17,22 +17,12 @@ def loading(bars: list[tuple[str, float]]):
         transient=True,
     ) as progress:
         tasks = []
-
-        # Tambahkan semua bar dengan teks dan durasi masing-masing
-        total_duration = sum(duration for _, duration in bars)
-        total_task = progress.add_task("[bold green]Memuat", total=100)
-
         for text, duration in bars:
             task = progress.add_task(f"[cyan]{text}", total=100)
             tasks.append((task, duration))
 
-        # Proses semua bar secara berurutan
         completed = 0
         for task, duration in tasks:
             for _ in range(100):
                 time.sleep(duration / 100)
                 progress.update(task, advance=1)
-
-                # Update progress untuk Total
-                completed += (duration / total_duration) / 100 * 100
-                progress.update(total_task, completed=completed)
