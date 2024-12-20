7ae8c8750b19044b9a4ab1a48fb3a4df41cfc7ed
Putra <70789166+Dachenxi@users.noreply.github.com>
2024-12-20 19:42:27 +0700

    Menambahkan Import Exception Handler splash screen

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+try:
+    import rich, mysql.connector
+except ModuleNotFoundError:
+    import subprocess
+    subprocess.check_call(["python", "-m", "pip", "install", "rich", "mysql-connector-python"])
+
 from rich import print
 from rich.console import Group
 from rich.columns import Columns
@@ -5,6 +11,9 @@ from rich.align import Align
 from datetime import datetime
 from app import menu
 from view import panel
+from database import create_database
+from database import connect_db
+from config import load_environ
 import os
 
 def clear():
@@ -12,6 +21,17 @@ def clear():
 
 jam = datetime.now()
 now = datetime.strftime(jam,"%d/%m/%Y %H:%M:%S")
+ascii_art = r''' .-') _                .-. .-')
+(  OO) )               \  ( OO )
+/     '._  .-'),-----. ,--. ,--.  .-'),-----.
+|'--...__)( OO'  .-.  '|  .'   / ( OO'  .-.  '
+'--.  .--'/   |  | |  ||      /, /   |  | |  |
+   |  |   \_) |  |\|  ||     ' _)\_) |  |\|  |
+   |  |     \ |  | |  ||  .   \    \ |  | |  |
+   |  |      `'  '-'  '|  |\   \    `'  '-'  '
+   `--'        `-----' `--' '--'      `-----'
+'''
+
 kolom_title = Columns([
     Align.left("Interface Untuk Database Toko Kelontong"),
     Align.right(f"{now}")
@@ -21,11 +41,30 @@ kolom_ending = Columns([
     Align.left("Terima kasih sudah menggunakan program ini"),
     Align.right(f"{now}")
     ],expand=True)
-changelog = "Version 1.0"
+changelog = """Version 1.0
+> Initial Release
+> 1. View All Table
+> 2. View Barang
+> 3. View Supplier
+
+> Jangan berharap berjalan dengan lancar karena masih dalam tahap pengembangan
+> Menemukan bug? Silahkan hubungi developer
+> Terima kasih
+"""
 grub_main_menu = Group(panel(kolom_title),
+                       panel(Align.center(ascii_art)),
                        panel(changelog),
                        fit=False)
 grub_ending = Group(panel(kolom_ending))
+
+config = load_environ()
+
+db, cursor = connect_db(config)
+
+try:
+    cursor.execute("SELECT * FROM barang")
+except:
+    create_database()
 
 try:
     clear()
