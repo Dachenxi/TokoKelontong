4beb637ecb3d6ddfe244358d54cba341ecc7b355
Putra <70789166+Dachenxi@users.noreply.github.com>
2024-12-20 19:45:51 +0700

    making it more easy to make config

--- config/config.py
+++ config/config.py
@@ -1,6 +1,10 @@
 import dotenv, sys, os
 from dotenv import load_dotenv
+from view import panel, tanya
+from rich import print
 
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 def load_environ():
     """
     Memuat konfigurasi database dari file .env dan mengembalikannya sebagai dictionary.
@@ -24,6 +28,24 @@ def load_environ():
     }
 
     if "" in config.values() or None in config.values():
-        raise ValueError("Beberapa kredensial database tidak ditemukan. Pastikan file .env sudah diatur dengan benar.")
+        clear()
+        print(panel("Masukan Database Host ",subtitle="╭─",subtitle_align="left"))
+        dbhost = tanya(str)
+        print(panel("Masukan Username",subtitle="╭─",subtitle_align="left"))
+        username = tanya(str)
+        print(panel("Masukan Password",subtitle="╭─",subtitle_align="left"))
+        password = tanya(str)
+
+        config_folder = "config"
+        config_file = ".env"
+        if not os.path.exists(config_folder):
+            os.makedirs(config_folder)
+        with open(f"{config_folder}/{config_file}","w") as f:
+            f.write(f"DB_HOST={dbhost}\n")
+            f.write(f"DB_USER={username}\n")
+            f.write(f"DB_PASSWORD={password}\n")
+            f.write("DB_DATABASE=tokokelontong\n")
+
+        load_environ()
     else:
         return config
